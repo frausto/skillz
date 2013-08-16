0d5ff36f45bbd4d6535324e1b5b9492254a0f173
Nolan Frausto <nrfrausto@gmail.com>
2013-08-15 20:05:10 -0400

    update sigma based on time decay

--- lib/skillz/player.rb
+++ lib/skillz/player.rb
@@ -1,12 +1,17 @@
 module Skillz
   class Player
 
-    attr_accessor :skill_level, :uncertainty_in_skill_level, :number_of_past_matches
+    attr_accessor :skill_level, :uncertainty_in_skill_level
 
-    def initialize(skill_level=nil, uncertainty=nil, past_matches=nil)
+    def initialize(skill_level=nil, uncertainty=nil, last_match_time=nil)
       @skill_level = skill_level || Skillz::INITIAL_SKILL_LEVEL
       @uncertainty_in_skill_level = uncertainty || Skillz::UNCERTAINTY
-      @number_of_past_matches = past_matches || 0
+      if last_match_time
+        days = (Time.now - last_match_time).to_i / (24 * 60 * 60)
+        time_decay = 1 + -Math.exp((days * -1.0)/365)
+        time_decay *= [[uncertainty_in_skill_level, 4].max, 10].min
+        @uncertainty_in_skill_level += time_decay
+      end
     end
   end
 end
--- spec/skillz/player_spec.rb
+++ spec/skillz/player_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
 
 describe Skillz::Player do
+  it "should increase uncertainty based on when the last match was played" do
+    now = Time.now
+    Time.stub(:now => now)
+    Skillz::Player.new(25, 4, now - 60.days).uncertainty_in_skill_level.round(7).should == 4.6063338
+    Skillz::Player.new(25, 4, now - 10.days).uncertainty_in_skill_level.round(7).should == 4.1081014
+  end
 end
