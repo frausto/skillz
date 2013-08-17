3bfa9d9f83c18e3924264b9137a847101c88dfa5
Nolan Frausto <nrfrausto@gmail.com>
2013-08-17 09:48:22 -0400

    dont adjust sigma for inactivity unless inactive for 25 days

--- lib/skillz/player.rb
+++ lib/skillz/player.rb
@@ -6,16 +6,21 @@ module Skillz
     def initialize(skill_level=nil, uncertainty=nil, last_match_time=nil)
       @skill_level = skill_level || Skillz::INITIAL_SKILL_LEVEL
       @skill_uncertainty = uncertainty || Skillz::UNCERTAINTY
-      if last_match_time
-        days = (Time.now - last_match_time).to_i / (24 * 60 * 60)
-        time_decay = 1 + -Math.exp((days * -1.0)/365)
-        time_decay *= [[skill_uncertainty, 4].max, 10].min
-        @skill_uncertainty += time_decay
-      end
+      adjust_for_inactivity(last_match_time) if last_match_time
     end
 
     def adjusted_skill_level
       @skill_level - (@skill_uncertainty * 0.5)
+    end
+
+    private
+
+    def adjust_for_inactivity(last_match_time)
+      days = (Time.now - last_match_time).to_i / (24 * 60 * 60)
+      return if days <= 25
+      time_decay = 1 - Math.exp((days * -1.0)/365)
+      time_decay *= [[skill_uncertainty, 4].max, 10].min
+      @skill_uncertainty += time_decay
     end
   end
 end
--- spec/skillz/player_spec.rb
+++ spec/skillz/player_spec.rb
@@ -1,29 +1,32 @@
 require 'spec_helper'
 
 describe Skillz::Player do
+  before do
+    @now = Time.now
+    Time.stub(:now => @now)
+  end
+
   it "should increase uncertainty based on when the last match was played" do
-    now = Time.now
-    Time.stub(:now => now)
-    Skillz::Player.new(25, 5, now - 60.days).skill_uncertainty.round(7).should == 5.7579173
-    Skillz::Player.new(25, 5, now - 10.days).skill_uncertainty.round(7).should == 5.1351268
+    Skillz::Player.new(25, 5, @now - 60.days).skill_uncertainty.round(7).should == 5.7579173
+  end
+
+  it "does not adjust if inactive for less than 25 days" do
+    Skillz::Player.new(25, 5, @now - 25.days).skill_uncertainty.round(7).should == 5
+    Skillz::Player.new(25, 5, @now - 26.days).skill_uncertainty.round(7).should == 5.343775
   end
 
    it "time decay assumes 4 for sigma lower than 4" do
-    now = Time.now
-    Time.stub(:now => now)
-    smaller = Skillz::Player.new(25, 2, now - 60.days).skill_uncertainty.round(7) - 2
-    larger = Skillz::Player.new(25, 4, now - 60.days).skill_uncertainty.round(7) - 4
+    smaller = Skillz::Player.new(25, 2, @now - 60.days).skill_uncertainty.round(7) - 2
+    larger = Skillz::Player.new(25, 4, @now - 60.days).skill_uncertainty.round(7) - 4
     smaller.should == larger
   end
 
   it "time decay assumes 10 for sigma larger than 10" do
-    now = Time.now
-    Time.stub(:now => now)
-    smaller = Skillz::Player.new(25, 11, now - 60.days).skill_uncertainty.round(7) - 11
-    larger = Skillz::Player.new(25, 10, now - 60.days).skill_uncertainty.round(7) - 10
+    smaller = Skillz::Player.new(25, 11, @now - 60.days).skill_uncertainty.round(7) - 11
+    larger = Skillz::Player.new(25, 10, @now - 60.days).skill_uncertainty.round(7) - 10
     smaller.should == larger
-    smaller = Skillz::Player.new(25, 13, now - 13.days).skill_uncertainty.round(7) - 13
-    larger = Skillz::Player.new(25, 10, now - 13.days).skill_uncertainty.round(7) - 10
+    smaller = Skillz::Player.new(25, 13, @now - 13.days).skill_uncertainty.round(7) - 13
+    larger = Skillz::Player.new(25, 10, @now - 13.days).skill_uncertainty.round(7) - 10
     smaller.should == larger
   end
 
